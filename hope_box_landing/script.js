94c2f9649f481aa1f44b69c417fde723727aea8a
mayte <maytepioli@gmail.com>
2025-03-12 22:27:54 -0300

    Add Dockerfile

---
+++ hope_box_landing/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.nav-item').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
