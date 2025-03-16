3024ae1d713a4fdb25c424a3ebf57730dab2c7d8
mayte <maytepioli@gmail.com>
2025-03-16 19:59:35 -0300

    Changes biuldddd

--- landing_page/build/web/flutter_bootstrap.js
+++ landing_page/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "636544234"
+    serviceWorkerVersion: "4098075712"
   }
 });
